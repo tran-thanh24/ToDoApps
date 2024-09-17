4158249738e5c58a2961ddf8bb716a8cf64e1087
tran-thanh24 <148622471+tran-thanh24@users.noreply.github.com>
2024-09-17 20:18:18 +0700

    commit

--- src/api/saga.ts
+++ src/api/saga.ts
@@ -1,4 +1,5 @@
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery, select } from "redux-saga/effects";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import {
   ADD_TODO_REQUEST,
@@ -13,23 +14,60 @@ import {
 } from "../redux/actionType";
 import { FETCH_TODOS, setTodos } from "../redux/actions";
 
+interface Todo {
+  id: number;
+  todo: string;
+}
+
+interface FetchTodosResponse {
+  data: Todo[];
+}
+
+interface AddTodoResponse {
+  data: Todo;
+}
+
 // Fetch todos
-const fetchTodosApi = (): Promise<{ data: any[] }> =>
-  axios.get("https://5ee04f989ed06d001696dd93.mockapi.io/blocks");
+const fetchTodosApi = (): Promise<FetchTodosResponse> =>
+  axios.get("https://5ee04f989ed06d001696dd93.mockapi.io/todo");
 
 // Add todo
-const addTodoApi = (todo: string): Promise<{ data: any }> =>
-  axios.post("https://5ee04f989ed06d001696dd93.mockapi.io/blocks", { todo });
+const addTodoApi = (todo: string): Promise<AddTodoResponse> =>
+  axios.post("https://5ee04f989ed06d001696dd93.mockapi.io/todo", { todo });
 
 // Edit todo
-const editTodoApi = (id: number, updatedTodo: string): Promise<{ data: any }> =>
-  axios.put(`https://5ee04f989ed06d001696dd93.mockapi.io/blocks/${id}`, {
+const editTodoApi = (
+  id: number,
+  updatedTodo: string
+): Promise<AddTodoResponse> =>
+  axios.put(`https://5ee04f989ed06d001696dd93.mockapi.io/todo/${id}`, {
     todo: updatedTodo,
   });
 
 // Delete todo
 const deleteTodoApi = (id: number): Promise<void> =>
-  axios.delete(`https://5ee04f989ed06d001696dd93.mockapi.io/blocks/${id}`);
+  axios.delete(`hhttps://5ee04f989ed06d001696dd93.mockapi.io/todo/${id}`);
+
+//Tải todos từ AsyncStorage
+function* loadTodosFromStorage() {
+  try {
+    const todos = yield call([AsyncStorage, "getItem"], "todos");
+    if (todos) {
+      yield put(setTodos(JSON.parse(todos)));
+    }
+  } catch (error) {
+    console.error("Error loading todos from AsyncStorage:", error);
+  }
+}
+
+//Lưu todos vào AsyncStorage
+function* saveTodosToStorage(todos: any[]) {
+  try {
+    yield call([AsyncStorage, "setItem"], "todos", JSON.stringify(todos));
+  } catch (error) {
+    console.error("Error saving todos to AsyncStorage:", error);
+  }
+}
 
 // Saga để fetch todos từ API
 function* fetchTodosSaga(): Generator<any, void, { data: any[] }> {
@@ -37,6 +75,7 @@ function* fetchTodosSaga(): Generator<any, void, { data: any[] }> {
     const response = yield call(fetchTodosApi);
     console.log("Fetched todos:", response.data);
     yield put(setTodos(response.data));
+    yield call(saveTodosToStorage, response.data);
   } catch (error) {
     console.error("Error fetching todos:", error);
   }
@@ -48,6 +87,8 @@ function* addTodoSaga(action: any): Generator<any, void, { data: any }> {
     const response = yield call(addTodoApi, action.payload);
     console.log("Todo added:", response.data);
     yield put({ type: ADD_TODO_SUCCESS, payload: response.data });
+    const todos: Todo[] = yield select((state: any) => state.todos);
+    yield call(saveTodosToStorage, [...todos, response.data]);
   } catch (error) {
     yield put({ type: ADD_TODO_FAILURE, payload: error.message });
   }
@@ -63,6 +104,12 @@ function* editTodoSaga(action: any): Generator<any, void, { data: any }> {
     );
     console.log("Todo edit:", response.data);
     yield put({ type: EDIT_TODO_SUCCESS, payload: response.data });
+
+    const todos: Todo[] = yield select((state: any) => state.todos);
+    const updatedTodo = todos.map((todo: Todo) =>
+      todo.id === response.data.id ? response.data : todo
+    );
+    yield call(saveTodosToStorage, updatedTodo);
   } catch (error) {
     yield put({ type: EDIT_TODO_FAILURE, payload: error.message });
   }
@@ -73,6 +120,12 @@ function* deleteTodoSaga(action: any): Generator<any, void, void> {
   try {
     yield call(deleteTodoApi, action.payload);
     yield put({ type: DELETE_TODO_SUCCESS, payload: action.payload });
+    const todos: Todo[] = yield select((state: any) => state.todos);
+    const updatedTodo = todos.filter(
+      (todo: Todo) => todo.id !== action.payload
+    );
+    yield call(saveTodosToStorage, updatedTodo);
+    console.log("Todo delete:", action.data);
   } catch (error) {
     yield put({ type: DELETE_TODO_FAILURE, payload: error.message });
   }
